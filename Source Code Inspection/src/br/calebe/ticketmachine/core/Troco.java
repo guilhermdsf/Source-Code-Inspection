e848121a29555901fffdb7699e6139323e0ffe98
joninm200 <78380697+joninm200@users.noreply.github.com>
2023-09-29 22:07:39 -0300

    erros-jonathan

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -11,38 +11,50 @@ class Troco {
     protected PapelMoeda[] papeisMoeda;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
-        }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+    papeisMoeda = new PapelMoeda[6];
+    int count = 0;
+
+    while (valor >= 100) {
+        valor -= 100;
+        count++;
     }
+    papeisMoeda[5] = new PapelMoeda(100, count);
+
+    count = 0;
+    while (valor >= 50) {
+        valor -= 50;
+        count++;
+    }
+    papeisMoeda[4] = new PapelMoeda(50, count);
+
+    count = 0;
+    while (valor >= 20) {
+        valor -= 20;
+        count++;
+    }
+    papeisMoeda[3] = new PapelMoeda(20, count);
+
+    count = 0;
+    while (valor >= 10) {
+        valor -= 10;
+        count++;
+    }
+    papeisMoeda[2] = new PapelMoeda(10, count);
+
+    count = 0;
+    while (valor >= 5) {
+        valor -= 5;
+        count++;
+    }
+    papeisMoeda[1] = new PapelMoeda(5, count);
+
+    count = 0;
+    while (valor >= 2) {
+        valor -= 2;
+        count++;
+    }
+    papeisMoeda[0] = new PapelMoeda(2, count);
+}
 
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);

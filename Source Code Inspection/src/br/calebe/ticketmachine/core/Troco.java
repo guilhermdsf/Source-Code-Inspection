98093b09ff17e63458305a1996c708552b1d83e6
Guilherme Ferreira <99925606+guilhermdsf@users.noreply.github.com>
2023-10-27 22:08:24 -0300

    Update Troco.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -12,66 +12,26 @@ class Troco {
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
-        }
+        int count;
+
+        count = valor / 100;
         papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
+
+        count = (valor % 100) / 50;
         papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
+
+        count = (valor % 50) / 20;
         papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
+
+        count = (valor % 20) / 10;
         papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
+
+        count = (valor % 10) / 5;
         papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-    }
-    papeisMoeda[4] = new PapelMoeda(50, count);
 
-    count = 0;
-    while (valor >= 20) {
-        valor -= 20;
-        count++;
+        count = (valor % 5) / 2;
+        papeisMoeda[0] = new PapelMoeda(2, count);
     }
-    papeisMoeda[3] = new PapelMoeda(20, count);
-
-    count = 0;
-    while (valor >= 10) {
-        valor -= 10;
-        count++;
-    }
-    papeisMoeda[2] = new PapelMoeda(10, count);
-
-    count = 0;
-    while (valor >= 5) {
-        valor -= 5;
-        count++;
-    }
-    papeisMoeda[1] = new PapelMoeda(5, count);
-
-    count = 0;
-    while (valor >= 2) {
-        valor -= 2;
-        count++;
-    }
-    papeisMoeda[0] = new PapelMoeda(2, count);
-}
 
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);
@@ -80,36 +40,29 @@ class Troco {
     class TrocoIterator implements Iterator<PapelMoeda> {
 
         protected Troco troco;
+        private int currentIdx;
 
         public TrocoIterator(Troco troco) {
             this.troco = troco;
+            this.currentIdx = 5;
         }
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i--) {
-                if (troco.papeisMoeda[i] != null) {
-                    return true;
-                }
-            }
-            return false;
+            return currentIdx >= 0 && troco.papeisMoeda[currentIdx] != null;
         }
 
         @Override
         public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
+            if (hasNext()) {
+                return troco.papeisMoeda[currentIdx--];
             }
-            return ret;
+            return null;
         }
 
         @Override
         public void remove() {
-            next();
+            // Não implementado
         }
     }
 }
